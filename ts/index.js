413122a75e2cbee0c413c6bfb3dbfad348f56dc7
”student3” <aleksej.ivanov2003@gmail.com>
2023-09-12 22:57:35 +0300

    error_afer_restore

--- ts/index.js
+++ ts/index.js
@@ -1,3 +1,12 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var openCard = function (form, saveCard) {
     form.style.display = "block";
     saveCard.style.display = "none";
@@ -107,21 +116,22 @@ var setEditingCard = function (editingCardId, cardId) {
     editingCardId[0] = cardId;
 };
 var saveDataToLocalStorage = function (data) {
-    localStorage.setItem("taskData", JSON.stringify(data));
+    if (typeof localStorage !== "undefined") {
+        localStorage.setItem("taskData", JSON.stringify(data));
+    }
 };
 var init = function () {
-    var storedData = localStorage.getItem("taskData");
-    var data;
-    if (storedData) {
-        data = JSON.parse(storedData);
-    }
-    else {
-        data = {
-            toDo: [],
-            inProgress: [],
-            done: [],
-            deleted: [],
-        };
+    var data = {
+        toDo: [],
+        inProgress: [],
+        done: [],
+        deleted: [],
+    };
+    if (typeof localStorage !== "undefined") {
+        var storedData = localStorage.getItem("taskData");
+        if (storedData) {
+            data = JSON.parse(storedData);
+        }
     }
     drawCard(data, "toDo");
     drawCard(data, "inProgress");
@@ -129,6 +139,7 @@ var init = function () {
     drawCard(data, "deleted");
     console.log(data);
     var editingCardId = [null];
+    //Todo column
     var ToDoform = document.querySelector("#todo-form-card");
     var addToDoButton = document.querySelector("#toDo-add-card");
     var cancelToDoButton = document.querySelector("#toDo-cancel-card");
@@ -154,13 +165,17 @@ var init = function () {
         event.preventDefault();
         addNewCard(data, toDotextarea, editingCardId, event);
         closeCard(ToDoform, addToDoButton, saveToDoCard, toDotextarea);
-        saveDataToLocalStorage(data);
+        if (typeof localStorage !== "undefined") {
+            saveDataToLocalStorage(data);
+        }
     });
     toDoList.addEventListener("click", function (event) {
         var target = event.target;
         if (target.classList.contains("delete-icon")) {
             deleteHandler(event, data, "toDo");
-            saveDataToLocalStorage(data);
+            if (typeof localStorage !== "undefined") {
+                saveDataToLocalStorage(data);
+            }
         }
         if (target.closest(".toDo-list-item")) {
             var cardIdElement = target.closest(".toDo-list-item");
@@ -183,6 +198,230 @@ var init = function () {
                 }
             }
         }
+    });
+    //Inprogress column
+    var inProgressForm = document.querySelector("#inProgress-form-card");
+    var addInProgressButton = document.querySelector("#inProgress-add-card");
+    var cancelInProgressButton = document.querySelector("#inProgress-cancel-card");
+    var saveInProgressCard = document.querySelector("#inProgress-add-item");
+    var inProgressTextarea = document.querySelector("#inProgress-textarea");
+    var InProgressList = document.querySelector("#inProgress-list");
+    inProgressForm.style.display = "none";
+    addInProgressButton.style.display = "none";
+    saveInProgressCard.style.display = "flex";
+    saveInProgressCard.addEventListener("click", function () {
+        openCard(inProgressForm, saveInProgressCard);
+    });
+    inProgressTextarea.addEventListener("input", function (event) {
+        checkOnValue(addInProgressButton, event);
+    });
+    cancelInProgressButton.addEventListener("click", function (event) {
+        inProgressTextarea.value = "";
+        inProgressForm.style.display = "none";
+        saveInProgressCard.style.display = "flex";
+        checkOnValue(addInProgressButton, event);
+    });
+    addInProgressButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        addNewCard(data, inProgressTextarea, editingCardId, event);
+        closeCard(inProgressForm, addInProgressButton, saveInProgressCard, inProgressTextarea);
+        if (typeof localStorage !== "undefined") {
+            saveDataToLocalStorage(data);
+        }
+    });
+    InProgressList.addEventListener("click", function (event) {
+        var target = event.target;
+        if (target.classList.contains("delete-icon")) {
+            deleteHandler(event, data, "inProgress");
+            if (typeof localStorage !== "undefined") {
+                saveDataToLocalStorage(data);
+            }
+        }
+        if (target.closest(".toDo-list-item")) {
+            var cardIdElement = target.closest(".toDo-list-item");
+            if (cardIdElement) {
+                var cardId_3 = +cardIdElement.id;
+                setEditingCard(editingCardId, cardId_3);
+                var card = data["inProgress"].find(function (_a) {
+                    var id = _a.id;
+                    return id === cardId_3;
+                });
+                var isCardOpen = cardIdElement.classList.contains("open");
+                if (isCardOpen) {
+                    closeCard(inProgressForm, addInProgressButton, saveInProgressCard, inProgressTextarea);
+                    cardIdElement.classList.remove("open");
+                }
+                else if (card) {
+                    inProgressTextarea.value = card.textarea;
+                    openCard(inProgressForm, saveInProgressCard);
+                    cardIdElement.classList.add("open");
+                }
+            }
+        }
+    });
+    //done column
+    var doneForm = document.querySelector("#done-form-card");
+    var addDoneButton = document.querySelector("#done-add-card");
+    var cancelDoneButton = document.querySelector("#done-cancel-card");
+    var saveDoneCard = document.querySelector("#done-add-item");
+    var doneTextarea = document.querySelector("#done-textarea");
+    var doneList = document.querySelector("#done-list");
+    doneForm.style.display = "none";
+    addDoneButton.style.display = "none";
+    saveDoneCard.style.display = "flex";
+    saveDoneCard.addEventListener("click", function () {
+        openCard(doneForm, saveDoneCard);
+    });
+    doneTextarea.addEventListener("input", function (event) {
+        checkOnValue(addDoneButton, event);
+    });
+    cancelDoneButton.addEventListener("click", function (event) {
+        doneTextarea.value = "";
+        doneForm.style.display = "none";
+        saveDoneCard.style.display = "flex";
+        checkOnValue(addDoneButton, event);
+    });
+    addDoneButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        addNewCard(data, doneTextarea, editingCardId, event);
+        closeCard(doneForm, addDoneButton, saveDoneCard, doneTextarea);
+        if (typeof localStorage !== "undefined") {
+            saveDataToLocalStorage(data);
+        }
+    });
+    doneList.addEventListener("click", function (event) {
+        var target = event.target;
+        if (target.classList.contains("delete-icon")) {
+            deleteHandler(event, data, "done");
+            if (typeof localStorage !== "undefined") {
+                saveDataToLocalStorage(data);
+            }
+        }
+        if (target.closest(".toDo-list-item")) {
+            var cardIdElement = target.closest(".toDo-list-item");
+            if (cardIdElement) {
+                var cardId_4 = +cardIdElement.id;
+                setEditingCard(editingCardId, cardId_4);
+                var card = data["done"].find(function (_a) {
+                    var id = _a.id;
+                    return id === cardId_4;
+                });
+                var isCardOpen = cardIdElement.classList.contains("open");
+                if (isCardOpen) {
+                    closeCard(doneForm, addDoneButton, saveDoneCard, doneTextarea);
+                    cardIdElement.classList.remove("open");
+                }
+                else if (card) {
+                    doneTextarea.value = card.textarea;
+                    openCard(doneForm, saveDoneCard);
+                    cardIdElement.classList.add("open");
+                }
+            }
+        }
+    });
+    //delete column
+    var deletedForm = document.querySelector("#deleted-form-card");
+    var addDeletedButton = document.querySelector("#deleted-add-card");
+    var cancelDeletedButton = document.querySelector("#deleted-cancel-card");
+    var saveDeletedCard = document.querySelector("#deleted-add-item");
+    var deletedTextarea = document.querySelector("#deleted-textarea");
+    var deletedList = document.querySelector("#deleted-list");
+    deletedForm.style.display = "none";
+    addDeletedButton.style.display = "none";
+    saveDeletedCard.style.display = "flex";
+    saveDeletedCard.addEventListener("click", function () {
+        openCard(deletedForm, saveDeletedCard);
+    });
+    deletedTextarea.addEventListener("input", function (event) {
+        checkOnValue(addDeletedButton, event);
+    });
+    cancelDeletedButton.addEventListener("click", function (event) {
+        deletedTextarea.value = "";
+        deletedForm.style.display = "none";
+        saveDeletedCard.style.display = "flex";
+        checkOnValue(addDeletedButton, event);
+    });
+    addDeletedButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        addNewCard(data, deletedTextarea, editingCardId, event);
+        closeCard(deletedForm, addDeletedButton, saveDeletedCard, deletedTextarea);
+        if (typeof localStorage !== "undefined") {
+            saveDataToLocalStorage(data);
+        }
+    });
+    deletedList.addEventListener("click", function (event) {
+        var target = event.target;
+        if (target.classList.contains("restore-button")) {
+            deleteHandler(event, data, "deleted");
+            if (typeof localStorage !== "undefined") {
+                saveDataToLocalStorage(data);
+            }
+        }
+        if (target.closest(".toDo-list-item")) {
+            var cardIdElement = target.closest(".toDo-list-item");
+            if (cardIdElement) {
+                var cardId_5 = +cardIdElement.id;
+                setEditingCard(editingCardId, cardId_5);
+                var card = data["deleted"].find(function (_a) {
+                    var id = _a.id;
+                    return id === cardId_5;
+                });
+                var isCardOpen = cardIdElement.classList.contains("open");
+                if (isCardOpen) {
+                    closeCard(deletedForm, addDeletedButton, saveDeletedCard, deletedTextarea);
+                    cardIdElement.classList.remove("open");
+                }
+                else if (card) {
+                    deletedTextarea.value = card.textarea;
+                    openCard(deletedForm, saveDeletedCard);
+                    cardIdElement.classList.add("open");
+                }
+            }
+        }
+    });
+    //drap and drop
+    var itemContainers = __spreadArray([], document.querySelectorAll(".toDo-list"), true);
+    itemContainers.forEach(function (container) {
+        container.addEventListener("dragstart", function (event) {
+            var _a;
+            var dragEvent = event;
+            (_a = dragEvent.dataTransfer) === null || _a === void 0 ? void 0 : _a.setData("text/plain", dragEvent.target.id);
+            dragEvent.target.classList.add("dragged");
+            setTimeout(function () {
+                dragEvent.target.classList.remove("dragged");
+            }, 100);
+        });
+        container.addEventListener("dragover", function (event) {
+            event.preventDefault();
+            container.classList.add("dragover");
+        });
+        container.addEventListener("dragleave", function () {
+            container.classList.remove("dragover");
+        });
+        container.addEventListener("drop", function (event) {
+            var _a;
+            event.preventDefault();
+            var cardId = event;
+            var draggedCardId = (_a = cardId.dataTransfer) === null || _a === void 0 ? void 0 : _a.getData("text/plain");
+            if (draggedCardId) {
+                var draggedCard = document.getElementById(draggedCardId);
+                if (draggedCard && draggedCard.parentNode instanceof HTMLElement) {
+                    var targetCardItemId = container.id.split("-")[0];
+                    var sourceCardItemId = draggedCard.parentNode.id.split("-")[0];
+                    var cardIndex = data[sourceCardItemId].findIndex(function (_a) {
+                        var id = _a.id;
+                        return id === +draggedCardId;
+                    });
+                    var card = data[sourceCardItemId][cardIndex];
+                    data[sourceCardItemId].splice(cardIndex, 1);
+                    data[targetCardItemId].push(card);
+                    drawCard(data, sourceCardItemId);
+                    drawCard(data, targetCardItemId);
+                    saveDataToLocalStorage(data);
+                    container.classList.remove("dragover");
+                }
+            }
+        });
     });
 };
 init();
--- ts/index.ts
+++ ts/index.ts
@@ -35,8 +35,10 @@ const closeCard = (
   saveCard.style.display = "flex";
 };
 
-const drawCard = (data: TaskData, cardItemId: keyof TaskData) => {
-  const list = document.querySelector(`#${cardItemId}-item-container`);
+const drawCard = (data: TaskData, cardItemId: keyof TaskData): void => {
+  const list: Element | null = document.querySelector(
+    `#${cardItemId}-item-container`
+  );
   if (!list) return;
   list.innerHTML = "";
 
@@ -74,7 +76,7 @@ const addNewCard = (
   textarea: HTMLTextAreaElement,
   editingCardId: (number | null)[],
   event: Event
-) => {
+): void => {
   event.preventDefault();
   const cardItem = (event.target as HTMLTextAreaElement).closest(".card-item");
   if (cardItem) {
@@ -102,7 +104,7 @@ const deleteHandler = (
   event: Event,
   data: TaskData,
   cardItemId: keyof TaskData
-) => {
+): void => {
   const cardIdElement = (event.target as HTMLElement).closest(
     ".toDo-list-item"
   );
@@ -122,7 +124,7 @@ const deleteHandler = (
   }
 };
 
-const restoreCard = (data: TaskData, cardId: number) => {
+const restoreCard = (data: TaskData, cardId: number): void => {
   const cardIndex = data.deleted.findIndex(({ id }) => id === cardId);
 
   if (cardIndex !== -1) {
@@ -135,28 +137,33 @@ const restoreCard = (data: TaskData, cardId: number) => {
   }
 };
 
-const setEditingCard = (editingCardId: (number | null)[], cardId: number) => {
+const setEditingCard = (
+  editingCardId: (number | null)[],
+  cardId: number
+): void => {
   editingCardId[0] = cardId;
 };
 
-const saveDataToLocalStorage = (data: TaskData) => {
-  localStorage.setItem("taskData", JSON.stringify(data));
+const saveDataToLocalStorage = (data: TaskData): void => {
+  if (typeof localStorage !== "undefined") {
+    localStorage.setItem("taskData", JSON.stringify(data));
+  }
 };
 
-const init = () => {
-  const storedData = localStorage.getItem("taskData");
+const init = (): void => {
+  let data: TaskData = {
+    toDo: [],
+    inProgress: [],
+    done: [],
+    deleted: [],
+  };
 
-  let data: TaskData;
+  if (typeof localStorage !== "undefined") {
+    const storedData = localStorage.getItem("taskData");
 
-  if (storedData) {
-    data = JSON.parse(storedData);
-  } else {
-    data = {
-      toDo: [],
-      inProgress: [],
-      done: [],
-      deleted: [],
-    };
+    if (storedData) {
+      data = JSON.parse(storedData);
+    }
   }
 
   drawCard(data, "toDo");
@@ -166,7 +173,7 @@ const init = () => {
 
   console.log(data);
   const editingCardId: (number | null)[] = [null];
-
+  //Todo column
   const ToDoform = document.querySelector("#todo-form-card") as HTMLDivElement;
   const addToDoButton = document.querySelector(
     "#toDo-add-card"
@@ -205,14 +212,20 @@ const init = () => {
     event.preventDefault();
     addNewCard(data, toDotextarea, editingCardId, event);
     closeCard(ToDoform, addToDoButton, saveToDoCard, toDotextarea);
-    saveDataToLocalStorage(data);
+
+    if (typeof localStorage !== "undefined") {
+      saveDataToLocalStorage(data);
+    }
   });
 
   toDoList.addEventListener("click", (event) => {
     const target = event.target as HTMLElement;
     if (target.classList.contains("delete-icon")) {
       deleteHandler(event, data, "toDo");
-      saveDataToLocalStorage(data);
+
+      if (typeof localStorage !== "undefined") {
+        saveDataToLocalStorage(data);
+      }
     }
 
     if (target.closest(".toDo-list-item")) {
@@ -233,6 +246,314 @@ const init = () => {
         }
       }
     }
+  });
+
+  //Inprogress column
+  const inProgressForm = document.querySelector(
+    "#inProgress-form-card"
+  ) as HTMLDivElement;
+  const addInProgressButton = document.querySelector(
+    "#inProgress-add-card"
+  ) as HTMLButtonElement;
+  const cancelInProgressButton = document.querySelector(
+    "#inProgress-cancel-card"
+  ) as HTMLButtonElement;
+  const saveInProgressCard = document.querySelector(
+    "#inProgress-add-item"
+  ) as HTMLButtonElement;
+  const inProgressTextarea = document.querySelector(
+    "#inProgress-textarea"
+  ) as HTMLTextAreaElement;
+  const InProgressList = document.querySelector(
+    "#inProgress-list"
+  ) as HTMLUListElement;
+
+  inProgressForm.style.display = "none";
+  addInProgressButton.style.display = "none";
+  saveInProgressCard.style.display = "flex";
+
+  saveInProgressCard.addEventListener("click", () => {
+    openCard(inProgressForm, saveInProgressCard);
+  });
+
+  inProgressTextarea.addEventListener("input", (event) => {
+    checkOnValue(addInProgressButton, event);
+  });
+
+  cancelInProgressButton.addEventListener("click", (event) => {
+    inProgressTextarea.value = "";
+    inProgressForm.style.display = "none";
+    saveInProgressCard.style.display = "flex";
+    checkOnValue(addInProgressButton, event);
+  });
+
+  addInProgressButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    addNewCard(data, inProgressTextarea, editingCardId, event);
+    closeCard(
+      inProgressForm,
+      addInProgressButton,
+      saveInProgressCard,
+      inProgressTextarea
+    );
+
+    if (typeof localStorage !== "undefined") {
+      saveDataToLocalStorage(data);
+    }
+  });
+
+  InProgressList.addEventListener("click", (event) => {
+    const target = event.target as HTMLElement;
+    if (target.classList.contains("delete-icon")) {
+      deleteHandler(event, data, "inProgress");
+
+      if (typeof localStorage !== "undefined") {
+        saveDataToLocalStorage(data);
+      }
+    }
+
+    if (target.closest(".toDo-list-item")) {
+      const cardIdElement = target.closest(".toDo-list-item");
+      if (cardIdElement) {
+        const cardId = +cardIdElement.id;
+        setEditingCard(editingCardId, cardId);
+
+        const card = data["inProgress"].find(({ id }) => id === cardId);
+        const isCardOpen = cardIdElement.classList.contains("open");
+        if (isCardOpen) {
+          closeCard(
+            inProgressForm,
+            addInProgressButton,
+            saveInProgressCard,
+            inProgressTextarea
+          );
+          cardIdElement.classList.remove("open");
+        } else if (card) {
+          inProgressTextarea.value = card.textarea;
+          openCard(inProgressForm, saveInProgressCard);
+          cardIdElement.classList.add("open");
+        }
+      }
+    }
+  });
+
+  //done column
+  const doneForm = document.querySelector("#done-form-card") as HTMLDivElement;
+  const addDoneButton = document.querySelector(
+    "#done-add-card"
+  ) as HTMLButtonElement;
+  const cancelDoneButton = document.querySelector(
+    "#done-cancel-card"
+  ) as HTMLButtonElement;
+  const saveDoneCard = document.querySelector(
+    "#done-add-item"
+  ) as HTMLButtonElement;
+  const doneTextarea = document.querySelector(
+    "#done-textarea"
+  ) as HTMLTextAreaElement;
+  const doneList = document.querySelector("#done-list") as HTMLUListElement;
+
+  doneForm.style.display = "none";
+  addDoneButton.style.display = "none";
+  saveDoneCard.style.display = "flex";
+
+  saveDoneCard.addEventListener("click", () => {
+    openCard(doneForm, saveDoneCard);
+  });
+
+  doneTextarea.addEventListener("input", (event) => {
+    checkOnValue(addDoneButton, event);
+  });
+
+  cancelDoneButton.addEventListener("click", (event) => {
+    doneTextarea.value = "";
+    doneForm.style.display = "none";
+    saveDoneCard.style.display = "flex";
+    checkOnValue(addDoneButton, event);
+  });
+
+  addDoneButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    addNewCard(data, doneTextarea, editingCardId, event);
+    closeCard(doneForm, addDoneButton, saveDoneCard, doneTextarea);
+
+    if (typeof localStorage !== "undefined") {
+      saveDataToLocalStorage(data);
+    }
+  });
+
+  doneList.addEventListener("click", (event) => {
+    const target = event.target as HTMLElement;
+    if (target.classList.contains("delete-icon")) {
+      deleteHandler(event, data, "done");
+
+      if (typeof localStorage !== "undefined") {
+        saveDataToLocalStorage(data);
+      }
+    }
+
+    if (target.closest(".toDo-list-item")) {
+      const cardIdElement = target.closest(".toDo-list-item");
+      if (cardIdElement) {
+        const cardId = +cardIdElement.id;
+        setEditingCard(editingCardId, cardId);
+
+        const card = data["done"].find(({ id }) => id === cardId);
+        const isCardOpen = cardIdElement.classList.contains("open");
+        if (isCardOpen) {
+          closeCard(doneForm, addDoneButton, saveDoneCard, doneTextarea);
+          cardIdElement.classList.remove("open");
+        } else if (card) {
+          doneTextarea.value = card.textarea;
+          openCard(doneForm, saveDoneCard);
+          cardIdElement.classList.add("open");
+        }
+      }
+    }
+  });
+  //delete column
+  const deletedForm = document.querySelector(
+    "#deleted-form-card"
+  ) as HTMLDivElement;
+  const addDeletedButton = document.querySelector(
+    "#deleted-add-card"
+  ) as HTMLButtonElement;
+  const cancelDeletedButton = document.querySelector(
+    "#deleted-cancel-card"
+  ) as HTMLButtonElement;
+  const saveDeletedCard = document.querySelector(
+    "#deleted-add-item"
+  ) as HTMLButtonElement;
+  const deletedTextarea = document.querySelector(
+    "#deleted-textarea"
+  ) as HTMLTextAreaElement;
+  const deletedList = document.querySelector(
+    "#deleted-list"
+  ) as HTMLUListElement;
+
+  deletedForm.style.display = "none";
+  addDeletedButton.style.display = "none";
+  saveDeletedCard.style.display = "flex";
+
+  saveDeletedCard.addEventListener("click", () => {
+    openCard(deletedForm, saveDeletedCard);
+  });
+
+  deletedTextarea.addEventListener("input", (event) => {
+    checkOnValue(addDeletedButton, event);
+  });
+
+  cancelDeletedButton.addEventListener("click", (event) => {
+    deletedTextarea.value = "";
+    deletedForm.style.display = "none";
+    saveDeletedCard.style.display = "flex";
+    checkOnValue(addDeletedButton, event);
+  });
+
+  addDeletedButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    addNewCard(data, deletedTextarea, editingCardId, event);
+    closeCard(deletedForm, addDeletedButton, saveDeletedCard, deletedTextarea);
+
+    if (typeof localStorage !== "undefined") {
+      saveDataToLocalStorage(data);
+    }
+  });
+
+  deletedList.addEventListener("click", (event) => {
+    const target = event.target as HTMLElement;
+    if (target.classList.contains("restore-button")) {
+      deleteHandler(event, data, "deleted");
+
+      if (typeof localStorage !== "undefined") {
+        saveDataToLocalStorage(data);
+      }
+    }
+
+    if (target.closest(".toDo-list-item")) {
+      const cardIdElement = target.closest(".toDo-list-item");
+      if (cardIdElement) {
+        const cardId = +cardIdElement.id;
+        setEditingCard(editingCardId, cardId);
+
+        const card = data["deleted"].find(({ id }) => id === cardId);
+        const isCardOpen = cardIdElement.classList.contains("open");
+        if (isCardOpen) {
+          closeCard(
+            deletedForm,
+            addDeletedButton,
+            saveDeletedCard,
+            deletedTextarea
+          );
+          cardIdElement.classList.remove("open");
+        } else if (card) {
+          deletedTextarea.value = card.textarea;
+          openCard(deletedForm, saveDeletedCard);
+          cardIdElement.classList.add("open");
+        }
+      }
+    }
+  });
+  //drap and drop
+  const itemContainers: Element[] = [
+    ...document.querySelectorAll(".toDo-list"),
+  ];
+  itemContainers.forEach((container) => {
+    container.addEventListener("dragstart", (event) => {
+      const dragEvent = event as DragEvent;
+      dragEvent.dataTransfer?.setData(
+        "text/plain",
+        (dragEvent.target as HTMLElement).id
+      );
+      (dragEvent.target as HTMLElement).classList.add("dragged");
+
+      setTimeout(() => {
+        (dragEvent.target as HTMLElement).classList.remove("dragged");
+      }, 100);
+    });
+
+    container.addEventListener("dragover", (event) => {
+      event.preventDefault();
+      container.classList.add("dragover");
+    });
+
+    container.addEventListener("dragleave", () => {
+      container.classList.remove("dragover");
+    });
+
+    container.addEventListener("drop", (event) => {
+      event.preventDefault();
+      const cardId = event as DragEvent;
+      const draggedCardId: string | undefined =
+        cardId.dataTransfer?.getData("text/plain");
+      if (draggedCardId) {
+        const draggedCard = document.getElementById(draggedCardId);
+
+        if (draggedCard && draggedCard.parentNode instanceof HTMLElement) {
+          const targetCardItemId = container.id.split("-")[0];
+          const sourceCardItemId = draggedCard.parentNode.id.split("-")[0];
+
+          const cardIndex = (
+            data[sourceCardItemId as keyof TaskData] as Task[]
+          ).findIndex(({ id }) => id === +draggedCardId);
+          const card = (data[sourceCardItemId as keyof TaskData] as Task[])[
+            cardIndex
+          ];
+
+          (data[sourceCardItemId as keyof TaskData] as Task[]).splice(
+            cardIndex,
+            1
+          );
+          (data[targetCardItemId as keyof TaskData] as Task[]).push(card);
+
+          drawCard(data, sourceCardItemId as keyof TaskData);
+          drawCard(data, targetCardItemId as keyof TaskData);
+          saveDataToLocalStorage(data);
+
+          container.classList.remove("dragover");
+        }
+      }
+    });
   });
 };
 
